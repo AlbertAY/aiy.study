793a1046448c1f0c04b0baa836b2b70ee8be3500
aiy <ay_1013@outlook.com>
2020-09-25 10:26:13 +0800

    kafka的demo环境演示

--- src/002_KAFKA/MyKAFKA.Consumer.Client/Program.cs
+++ src/002_KAFKA/MyKAFKA.Consumer.Client/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Confluent.Kafka;
+using System;
 
 namespace MyKAFKA.Consumer.Client
 {
     class Program
     {
+        const int cancellationToken = 100000000;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("这里是kafka消费者程序!");
+
+            ThisConsume();
+
+            Console.ReadLine();
         }
+
+        public static void ThisConsume() 
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "192.168.0.7:9092",//
+                GroupId = "foo",
+                //AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            bool cancelled = true;
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                //订阅，topic为 test的
+                consumer.Subscribe("test");
+
+                while (cancelled)
+                {
+                    ConsumeResult<Ignore, string> consumeResult = consumer.Consume(cancellationToken);
+
+                    string msg = $"接收到的值是：{consumeResult.Message.Value}  {DateTime.Now.ToString()}";
+
+                    Console.WriteLine(msg);
+                    
+                }
+                consumer.Close();
+            }
+
+        }
+
     }
 }
--- src/002_KAFKA/MyKAFKA.Producer.Client/Program.cs
+++ src/002_KAFKA/MyKAFKA.Producer.Client/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MyKAFKA.Producer.Client
 {
@@ -6,7 +10,39 @@ namespace MyKAFKA.Producer.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("这里是Kafka生产者程序!");
+
+            Task.Run(() =>
+            {
+                Send();
+            });            
+
+            Console.ReadLine();
+        }
+
+
+        public static async void Send()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "192.168.0.7:9092",//可以配置多个集群，逗号分隔
+            };
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                while (true)
+                {
+                    Thread.Sleep(5000);
+
+                    string msg = $"a log message {DateTime.Now.ToString()}";
+
+                    //发送topic为test的
+                    await producer.ProduceAsync("test", new Message<string, string> { Key = "test", Value = msg });
+
+                    Console.WriteLine($"发送的消息是 {msg}");
+                }
+                
+            }
         }
     }
 }
